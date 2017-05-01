b92cd60730f994b6ef6895079c8ca023c1aaaaed
Matthew Nelson <kb0mgq@gmail.com>
2017-04-30 21:37:28 -0500

    Final for RevA
    Final changes based on prototype testing for Rev A.

--- sw/keychain_fw/keychain_fw.ino
+++ sw/keychain_fw/keychain_fw.ino
@@ -5,29 +5,39 @@ int brightness = 0;  // variable used to set the brightness of tail beacon.
 int fadeAmount = 15;
 int count = 0;  //Initialize counter
 
+// Defines
+#define LED_Beacon    1
+#define LED_Tail      0
+#define LED_NAVL      3
+#define LED_NAVR      2
+#define LED_STROBE   4
+
 void setup() {
   // put your setup code here, to run once:
-  pinMode(3, OUTPUT);
-  pinMode(2, OUTPUT);
-  pinMode(0, OUTPUT);
+  pinMode(LED_Beacon, OUTPUT);
+  pinMode(LED_Tail, OUTPUT);
+  pinMode(LED_NAVL, OUTPUT);
+  pinMode(LED_NAVR, OUTPUT);
+  pinMode(LED_STROBE, OUTPUT);
+  digitalWrite(LED_NAVR,HIGH);
+  digitalWrite(LED_NAVL,HIGH);
+  analogWrite(LED_Tail,90);
 }
 
 void loop() {
   // put your main code here, to run repeatedly:
   if (count < 10){
-  digitalWrite(3,LOW);
-  digitalWrite(2,LOW);
+  digitalWrite(LED_STROBE,LOW);
   count++;
   }
   else
   {
-    digitalWrite(3,HIGH);
-    digitalWrite(2,HIGH);
+    digitalWrite(LED_STROBE,HIGH);
     delay(10);
     count = 0;
   }
   
-  analogWrite(0, brightness);
+  analogWrite(LED_Beacon, brightness);
   // change the brightness for next time through the loop:
   brightness = brightness + fadeAmount;
  
